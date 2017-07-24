16d59167345917e020bf01e433926e8bc7ff0d20
evelyn_zhang <1054982653@qq.com>
2017-07-24 16:25:43 +0800

    update map son

--- src/assets/js/map_follow.js
+++ src/assets/js/map_follow.js
@@ -8,79 +8,154 @@ var mapFollow = [
         src: '/src/dist/images/qrcode-follow/default.png'
     },
     {
-        channel: 'Weibo',
+        channel: 19,
         hmsr:'social_weibo',
         src: '/src/dist/images/qrcode-follow/19weibofollow.png'
     },
     {
-        channel: 'Search',
+        channel: 20,
         hmsr:'social_search',
         src: '/src/dist/images/qrcode-follow/20searchfollow.png'
     },
     {
-        channel: 'Banner ad - H5',
+        channel: 21,
         hmsr:'ad_banner',
         src: '/src/dist/images/qrcode-follow/21bannerfollow.png'
     },
     {
-        channel: 'Moments ad',
+        channel: 22,
         hmsr:'app_generalfollow',
         src: '/src/dist/images/qrcode-follow/22generalfollow.png'
     },
     {
-        channel: 'KOL 1',
+        channel: 23,
         hmsr:'ad_moments',
         src: '/src/dist/images/qrcode-follow/23momentsfollow.png'
     },
     {
-        channel: 'KOL 1',
+        channel: 24,
         hmsr:'ad_k1',
         src: '/src/dist/images/qrcode-follow/24kol1follow.png'
     },
     {
-        channel: 'KOL 3',
+        channel: 36,
+        hmsr:'ad_k2',
+        src: '/src/dist/images/qrcode-follow/36kol2follow.png'
+    },
+    {
+        channel: 38,
+        hmsr:'ad_k3',
+        src: '/src/dist/images/qrcode-follow/38kol3follow.png'
+    },
+    {
+        channel: 39,
+        hmsr:'ad_k4',
+        src: '/src/dist/images/qrcode-follow/39kol4follow.png'
+    },
+    {
+        channel: 25,
         hmsr:'ad_ma1',
         src: '/src/dist/images/qrcode-follow/25ma1follow.png'
     },
     {
-        channel: 'MA2',
+        channel: 26,
         hmsr:'ad_ma2',
         src: '/src/dist/images/qrcode-follow/26ma2follow.png'
     },
     {
-        channel: 'KOL 3',
-        hmsr:'ad_k3',
+        channel: 40,
+        hmsr:'ad_ma3',
+        src: '/src/dist/images/qrcode-follow/40ma3follow.png'
+    },
+    {
+        channel: 27,
+        hmsr:'app_post',
         src: '/src/dist/images/qrcode-follow/27postfollow.png'
     },
     {
-        channel: 'KOL 3',
-        hmsr:'app_post',
-        src: '/src/dist/images/qrcode-follow/28generalfollow.png'
-    },
-    {
-        channel: 'wechat menu',
-        hmsr:'app_menu',
-        src: '/src/dist/images/qrcode-follow/29menufollow.png'
-    },
-    {
-        channel: 'keywords',
+        channel: 30,
         hmsr:'app_keyword',
         src: '/src/dist/images/qrcode-follow/30keywordsfollow.png'
     },
     {
-        channel: 'KOL 3',
-        hmsr:'newsletter',
+        channel: 31,
+        hmsr:'app_newsletter',
         src: '/src/dist/images/qrcode-follow/31newsletterfollow.png'
     },
     {
-        channel: 'KOL 3',
-        hmsr:'ad_k3',
+        channel: 32,
+        hmsr:'app_hompage',
         src: '/src/dist/images/qrcode-follow/32homepagefollow.png'
     },
     {
-        channel: 'KOL 3',
-        hmsr:'ad_k3',
+        channel: 33,
+        hmsr:'app_retail1',
         src: '/src/dist/images/qrcode-follow/33retail1follow.png'
+    },
+    {
+        channel: 41,
+        hmsr:'app_retail2',
+        src: '/src/dist/images/qrcode-follow/41retail2follow.png'
+    },
+    {
+        channel: 42,
+        hmsr:'app_retail3',
+        src: '/src/dist/images/qrcode-follow/42retail3follow.png'
+    },
+    {
+        channel: 43,
+        hmsr:'app_retail4',
+        src: '/src/dist/images/qrcode-follow/43retail4follow.png'
+    },
+    {
+        channel: 44,
+        hmsr:'app_retail5',
+        src: '/src/dist/images/qrcode-follow/44retail5follow.png'
+    },
+    {
+        channel: 45,
+        hmsr:'app_retail6',
+        src: '/src/dist/images/qrcode-follow/45retail6follow.png'
+    },
+    {
+        channel: 46,
+        hmsr:'app_retail7',
+        src: '/src/dist/images/qrcode-follow/46retail7follow.png'
+    },
+    {
+        channel: 47,
+        hmsr:'app_retail8',
+        src: '/src/dist/images/qrcode-follow/47retail8follow.png'
+    },
+    {
+        channel: 48,
+        hmsr:'app_retail9',
+        src: '/src/dist/images/qrcode-follow/48retail9follow.png'
+    },
+    {
+        channel: 49,
+        hmsr:'app_retail10',
+        src: '/src/dist/images/qrcode-follow/49retail10follow.png'
+    },
+    {
+        channel: 50,
+        hmsr:'app_retail11',
+        src: '/src/dist/images/qrcode-follow/50retail11follow.png'
+    },
+    {
+        channel: 51,
+        hmsr:'app_retail12',
+        src: '/src/dist/images/qrcode-follow/51retail12follow.png'
+    },
+    {
+        channel: 52,
+        hmsr:'app_retail13',
+        src: '/src/dist/images/qrcode-follow/52retail13follow.png'
+    },
+    {
+        channel: 34,
+        hmsr:'app_share',
+        src: '/src/dist/images/qrcode-follow/34sharefollow.png'
     },
 
 ]
